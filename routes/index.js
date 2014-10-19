24fed26beae6d5772f79345fe1884af1918d251c
Alvaro Atienza <alv.atienza@gmail.com>
2014-10-19 14:14:41 -0400

    mobilizing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express', test: 'bang' });
+	var panels = [
+					{
+					 'type': 'Olympic Weightlifting',
+					 'class': 'oly',
+					 'img': '/snatch.jpg'
+					},
+					{
+					 'type': 'Powerlifting',
+					 'class': 'powerlifting',
+					 'img': '/tom.jpg'
+					},
+					{
+					 'type': 'Bodybuilding',
+					 'class': 'bodybuilding',
+					 'img': '/bicep.jpg'
+					},
+					{
+					 'type': 'Strength & Conditioning',
+					 'class': 'strenth_conditioning',
+					 'img': '/strength.png'
+					}
+				];
+
+	res.render('index', { title: 'Marble Weightlifting', panels: panels });
+});
+
+router.get('/club', function(req, res) {
+	res.render('club', { title: 'Marble Weightlifting'});
 });
 
 module.exports = router;
